b185bb847f522f072e0fb10991b9471f3deaae9d
Jaki Hawker <jmichel910@yahoo.com>
2021-12-02 22:28:04 -0700

    README updated

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ window.addEventListener('load', () => {
   // Accesing Geolocation
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
-      // Storing Longitude and Latitude in variables
+      // Longitude and Latitude variables
       long = position.coords.longitude;
       lat = position.coords.latitude;
       const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
@@ -26,12 +26,11 @@ window.addEventListener('load', () => {
           return response.json();
         })
         .then((data) => {
-          const { temp, feels_like } = data.main;
+          const { temp } = data.main;
           const place = data.name;
           const { description, icon } = data.weather[0];
           const { sunrise, sunset } = data.sys;
 
-          const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
           const fahrenheit = (temp * 9) / 5 + 32;
 
           const sunriseGMT = new Date(sunrise * 1000);
